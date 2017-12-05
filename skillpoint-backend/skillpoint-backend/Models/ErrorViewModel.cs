b2165c57723d7ea47ffd26da2c8c07a2dd72a94c
Bogdan Lupu <bogdanlupu13@yahoo.ro>
2017-12-05 12:20:39 +0200

    Created .NET project and integrated it with the Angular one

---
+++ skillpoint-backend/skillpoint-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace skillpoint_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
