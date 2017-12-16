ebf7c57cac0c4e4631eb8feb2335f726935a8227
Ioana <ioasitaru@yahoo.com>
2017-12-17 00:39:37 +0200

    Added footer component- Ioana Sitaru & Bogdan Lupu

--- skillpoint-frontend/Skillpoint/src/app/app.module.ts
+++ skillpoint-frontend/Skillpoint/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    IntroSectionComponent
+    IntroSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
